d3f155d374dd5134226b6fef2f67a4984168b301
Rajkumar Palaniyappan <rpalaniyappan@adventintermodal.com>
2024-06-16 23:19:30 -0700

    added basic commands

---
+++ src/akv-cli/KeyVaultConfig.cs
@@ -0,0 +1,10 @@
+namespace akv_cli
+{
+    public class KeyVaultConfig
+    {
+        public string KeyVaultUri { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
